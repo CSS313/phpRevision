809b50e758e6bbac4296c7eddb28be3d9e88b351
css313 <singh.chetan.313@gmail.com>
2024-12-21 23:11:10 +0530

    add some code for order form processing

--- bob's auto parts/processorder.php
+++ bob's auto parts/processorder.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
+<?php
+// create short variable names
+$tireqty = $_POST['tireqty'];
+$oilqty = $_POST['oilqty'];
+$sparkqty = $_POST['sparkqty'];
+?>
 <html>
 
 <head>
@@ -10,6 +16,10 @@
     <h2>Order Results</h2>
     <?php
     echo "<p>Order processed at " . date('H:i, jS F Y') . "</p>";
+    echo '<p>Your order is as follows: </p>';
+    echo htmlspecialchars($tireqty) . ' tires<br />';
+    echo htmlspecialchars($oilqty) . ' bottles of oil<br />';
+    echo htmlspecialchars(string: $sparkqty) . ' spark plugs<br />';
     ?>
 </body>
 
