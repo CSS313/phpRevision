5aa5107344924b0b15c0c9543359e95a394e73fe
css313 <singh.chetan.313@gmail.com>
2024-12-22 22:08:29 +0530

    add some code to calculate total amount with tax

--- bob's auto parts/processorder.php
+++ bob's auto parts/processorder.php
@@ -20,6 +20,21 @@ $sparkqty = $_POST['sparkqty'];
     echo htmlspecialchars($tireqty) . ' tires<br />';
     echo htmlspecialchars($oilqty) . ' bottles of oil<br />';
     echo htmlspecialchars(string: $sparkqty) . ' spark plugs<br />';
+
+    $totalqty = 0;
+    $totalqty = $tireqty + $oilqty + $sparkqty;
+    echo "<p>Items ordered: " . $totalqty . "<br />";
+    $totalamount = 0.00;
+    define('TIREPRICE', 100);
+    define('OILPRICE', 10);
+    define('SPARKPRICE', 4);
+    $totalamount = $tireqty * TIREPRICE
+        + $oilqty * OILPRICE
+        + $sparkqty * SPARKPRICE;
+    echo "Subtotal: $" . number_format($totalamount, 2) . "<br />";
+    $taxrate = 0.10; // local sales tax is 10%
+    $totalamount = $totalamount * (1 + $taxrate);
+    echo "Total including tax: $" . number_format($totalamount, 2) . "</p>";
     ?>
 </body>
 
