ce317c6c47ac1655065dc7b4699a97039b5dc1f9
css313 <singh.chetan.313@gmail.com>
2024-12-23 23:14:54 +0530

    minor changes in orderform.html and replaced nested if statements with switch in processorder.php

--- bob's auto parts/processorder.php
+++ bob's auto parts/processorder.php
@@ -36,18 +36,24 @@ $find = $_POST['find'];
     $taxrate = 0.10; // local sales tax is 10%
     $totalamount = $totalamount * (1 + $taxrate);
     echo "Total including tax: $" . number_format($totalamount, 2) . "</p>";
-    
-    if ($find == "a") {
-        echo "<p>Regular customer.</p>";
-        } elseif ($find == "b") {
-        echo "<p>Customer referred by TV advert.</p>";
-        } elseif ($find == "c") {
-        echo "<p>Customer referred by phone directory.</p>";
-        } elseif ($find == "d") {
-        echo "<p>Customer referred by word of mouth.</p>";
-        } else {
-        echo "<p>We do not know how this customer found us.</p>";
-        }
+
+    switch ($find) {
+        case "a":
+            echo "<p>Regular customer.</p>";
+            break;
+        case "b":
+            echo "<p>Customer referred by TV advert.</p>";
+            break;
+        case "c":
+            echo "<p>Customer referred by phone directory.</p>";
+            break;
+        case "d":
+            echo "<p>Customer referred by word of mouth.</p>";
+            break;
+        default:
+            echo "<p>We do not know how this customer found us.</p>";
+            break;
+    }
     ?>
 </body>
 
